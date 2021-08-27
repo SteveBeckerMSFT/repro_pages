6f3c1319c0dc189594873eb4e2bc7fcf2881a06e
Steve Becker <stevebe@microsoft.com>
2021-08-26 18:40:49 -0700

    V2

--- service_worker/load_image.js
+++ service_worker/load_image.js
@@ -3,3 +3,9 @@
 const image_element = document.createElement('img');
 image_element.src = 'image.png';
 document.body.appendChild(image_element);
+
+function load_image() {
+    const image_element = document.createElement('img');
+    image_element.src = 'image.png';
+    document.body.appendChild(image_element);
+}
