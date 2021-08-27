301901a4a82024dc1092e195a5607d3679209453
Steve Becker <stevebe@microsoft.com>
2021-08-26 17:36:08 -0700

    fix service worker

--- service_worker/service_worker.js
+++ service_worker/service_worker.js
@@ -1,6 +1,16 @@
 "use strict";
 
-onfetch = (fetchEvent) => {
-    console.log(fetchEvent.request.url);
-    fetchEvent.respondWith(fetch(fetchEvent.request));
+oninstall = (install_event) => {
+  install_event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        'load_image.js',
+        'image.png',
+      ]);
+    })
+  );
+}
+
+onfetch = (fetch_event) => {
+  fetch_event.respondWith(caches.match(fetch_event.request));
 }
