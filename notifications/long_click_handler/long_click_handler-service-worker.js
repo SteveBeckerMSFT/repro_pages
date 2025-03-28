517f9244e1c5a12f72291c0c456a0e56f7504d4d
Steve Becker <stevebe@microsoft.com>
2025-03-28 12:04:05 -0700

    Add notification long click handler repro

---
+++ notifications/long_click_handler/long_click_handler-service-worker.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const longTime = 2 * 60 * 1000; // 2 minutes.
+
+skipWaiting();
+
+onactivate = () => {
+  return clients.claim();
+}
+
+onnotificationclick = (event) => {
+  const notification = event.notification;
+
+  clients.matchAll()
+    .then((resultList) => {
+      resultList.forEach((client) => {
+        client.postMessage(`ServiceWorkerGlobalScope 'click' event for: ${notification.title} , timestamp: ${new Date(notification.timestamp)}, requireInteraction: ${notification.requireInteraction}, silent: ${notification.silent}`);
+      });
+    });
+
+  event.waitUntil(new Promise((resolve) => {
+
+    setTimeout(() => {
+      clients.openWindow('on-click.html');
+      resolve();
+    }, longTime);
+  }));
+};
+
+onnotificationclose = (event) => {
+  clients.matchAll()
+    .then((resultList) => {
+      resultList.forEach((client) => {
+        const notification = event.notification;
+        client.postMessage(`ServiceWorkerGlobalScope 'close' event for: ${notification.title} , timestamp: ${new Date(notification.timestamp)}, requireInteraction: ${notification.requireInteraction}, silent: ${notification.silent}`);
+      });
+    });
+
+  event.waitUntil(new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, longTime);
+  }));
+};
